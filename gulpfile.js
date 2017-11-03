1c3d7c17ac7da2cc90eb162a2038391e2b41f8a4
addis abebe <abiddas04@gmail.com>
2017-11-02 22:20:54 -0600

    adding compile, release and default task into gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,31 @@
 var gulp = require('gulp'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    typescript = require('gulp-typescript'),
+    small = require('small'),
+    sourcemaps = require('gulp-sourcemaps');
+
+var tsProject = typescript.createProject('lib/tsproject.json');
+
+gulp.task('compile', () => {
+    return gulp.src('lib/**/*.ts')
+        .pipe(sourcemaps.init())
+        .pipe(typescript(tsProject))
+        .pipe(small('index.js', {
+            externalResolve: ['node_modules'],
+            globalModules: {
+                "crypto": {
+                    standalone: "undefined"
+                }
+            }
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('static/scripts'));
+});
 
 gulp.task('release', ['compile'], () => {
     return gulp.src('static/scripts/scripts.js')
         .pipe(uglify())
         .pipe(gulp.dest('static/scripts'));
 });
+
+gulp.task('default', ['compile']);
