a7db3140244fc0a86e2db65bc9996b1a017bbd6c
addis abebe <abiddas04@gmail.com>
2017-11-03 18:45:25 -0600

    adding ForecastResponse anf ForecastItem

--- lib/api.ts
+++ lib/api.ts
@@ -11,3 +11,22 @@ export function getUrl(path: string) {
     url += "appid=" + openWeatherMapKey;
     return url;
 }
+
+export interface ForecastResponse {
+    city: {
+        name: string,
+        country: string;
+    };
+    list: ForecastItem[];
+}
+
+export interface ForecastItem {
+    dt: number;
+    main: {
+        temp: number;
+    };
+    weather: {
+        main: string;
+        description: string;
+    };
+}
